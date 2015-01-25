cb860706bb253fec764de99c4eb5d31e78a7599a
Stuxnet87 <anirudhc2013@email.iimcal.ac.in>
2015-01-26 04:27:47 +0530

    Cache Matrix v1
    Code to find inverse of matrix and return cached data if already
    existing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates a list of 4 functions to set matrix, get matrix, set inverse and get inverse of matrix
+## example: matsample<-makeCacheMatrix(); matsample$setMatrix(matrix variable here)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	    InvX <- NULL
+		## function sets the matrix & makes inverse null since matrix might change
+        setMatrix <- function(y) {
+                x <<- y
+                InvX <<- NULL
+        }
+        ## returns the matrix
+		getMatrix <- function() x
+		## called by cacheSolve to set the inverse of matrix and put it in InvX variable
+        setinv <- function(inv) InvX <<- inv
+		## used to get inverse of matrix set through setMatrix
+        getinv <- function() InvX
+		## list used to define the function calling names
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Computes the inverse of matrix, checks for cached data & shows that if present , examples usage: cacheSolve(matsample)
+cacheSolve <- function(x) {
+		## Check if the matrix inverse exists in cache	
+        InvX <- x$getinv()
+        if(!is.null(InvX)) {
+                message("Cached data received")
+                return(InvX)
+        }
+		## If matrix inverse doesn't exist in cache, then compute inverse
+		Mat=x$getMatrix()
+		## Flag checks if matrix is invertible by checking whether no of rows = no of columns and determinant of matrix is greater than 0
+		flag<-(nrow(Mat)==ncol(Mat))&(det(Mat)!=0)
+        ## If flag is true then invert matrix, otherwise show error message
+		if (flag)
+		{
+		  InvX <- solve(Mat)
+		}
+		else
+		{
+		  InvX<-"Matrix not invertible"
+		}
+        x$setinv(InvX)
+        InvX
 }
